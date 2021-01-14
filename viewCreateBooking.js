60f16e0a3430bc2cd9ed2c2b44e59bdc912f99c9
fluesmekk <theodortberg@gmail.com>
2021-01-14 12:32:23 +0100

    123

--- viewCreateBooking.js
+++ viewCreateBooking.js
@@ -1,1 +1,24 @@
-function 
+viewCreateBooking()
+function viewCreateBooking() {
+    let tableOverview = '';
+    let html = '';
+    html = `
+    <div class="page">
+        <div class="date">I dag: ${new Date()}</div>
+        <div class="inputs">
+            <div class="createName">Name: <input></div>
+            <div class="createNumber">Number: <input></div>
+            <div class="createDateAndTime"><input type="date"><input type="time"></div>
+            <div class="createGuests"><input type="range"><br>Antall gjester</div>
+            <div class="createTableInfo"></div>
+            <div class="createChildChair">Barnestol<input type="checkbox"></div>
+            <div class="createBooking"><button>Book</button></div>
+        </div>
+        <div class="tableOverview">
+            ${tableOverview}
+        </div>
+    </div>
+    `;
+    document.getElementById('app').innerHTML = html;
+}
+

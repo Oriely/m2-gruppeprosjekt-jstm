bae3495a626aef0a1cf4789f9bd491474167f8c9
Oriely <orielygaming@protonmail.com>
2021-01-15 15:06:10 +0100

    something

--- viewCreateBooking.js
+++ viewCreateBooking.js
@@ -1,6 +1,5 @@
 
 
-
 let tableOverview = '';
 let childBool = false;
 let rangeCount;
@@ -22,14 +21,13 @@ function viewCreateBooking() {
 
     let html = '';
     html = `
+
     <div class="page">
-        <div class="date">I dag: ${date}</div>
-        <div class="inputs">
-        <div></div>
-            <div class="tableInformation">
-                <div class="currentTable" id="check">Valgt Bord: <strong>${model.app.selectedTable}</strong></div>
-                <div class="maxTableGuests">Bordet har plass til: <strong>${rangeCount || 'Velg bord'}</strong>${rangeCount ? ' personer' : ''}</div>
-            </div>
+
+    <div class="date">I dag: ${date.toISOString()}</div>
+
+    <div class="inputs">
+        <div class="inputs-col1">
 
             <div class="createName"><div>Navn:</div> <input oninput="model.inputs.inputName = this.value" value="${model.inputs.inputName}"></input></div>
             <div class="createNumber"><div>Nummer:</div> <input oninput="model.inputs.inputNumber = this.value" value="${model.inputs.inputNumber}"></div>
@@ -41,36 +39,62 @@ function viewCreateBooking() {
             <div class="createTableInfo"></div>
             <div class="createChildChair">Barnestol<input type="checkbox" ${childBool ? 'checked' : ''} onclick="checkChildChair()" ></div>
             
-            <div class="createBooking"><button onclick="createBooking()">Book</button></div>
+            <div class="createBooking"><button onclick="createBooking()">Book</button></div>    
         </div>
-        <div class="tableDescription">
-        <div class="fourSeats">Bord med 4 plasser:</div><div class="sixSeats">Bord med 6 plasser:</div>
-        </div>
-        <div class="tableOverview">
         
-            <div class="a box" onclick="selectTable('a')">A</div>
-            <div class="b box" onclick="selectTable('b')">B</div>
-            <div class="c box" onclick="selectTable('c')">C</div>
-            <div class="d box" onclick="selectTable('d')">D</div>
-            <div class="e box" onclick="selectTable('e')">E</div>
-            <div class="f box" onclick="selectTable('f')">F</div>
-            <div class="g box" onclick="selectTable('g')">G</div>
-            <div class="h box" onclick="selectTable('h')">H</div>
-            <div class="i box" onclick="selectTable('i')">I</div>
-            <div class="j box" onclick="selectTable('j')">J</div>
-            <div class="k box" onclick="selectTable('k')">K</div>
-            <div class="l box" onclick="selectTable('l')">L</div>
-            <div class="m box" onclick="selectTable('m')">M</div>
-            <div class="n box" onclick="selectTable('n')">N</div>
-            <div class="o box" onclick="selectTable('o')">O</div>
-            <div class="p box" onclick="selectTable('p')">P</div>
-            <div class="q box" onclick="selectTable('q')">Q</div>
-            <div class="r box" onclick="selectTable('r')">R</div>
-            <div class="s box" onclick="selectTable('s')">S</div>
-            <div class="t box" onclick="selectTable('t')">T</div>
+        <div class="inputs-col2">
             
+        <div class="tableInformation">
+        <div class="currentTable" id="check">Valgt Bord: <strong>${model.app.selectedTable}</strong></div>
+        <div class="maxTableGuests">Bordet har plass til: ${rangeCount || '<i>Velg bord</i>'}${rangeCount ? ' personer' : ''}</div>
+    </div>
+
         </div>
     </div>
+
+
+    <div class="tableDescription">
+    <div class="fourSeats">Bord med 4 plasser:</div>
+    <div class="sixSeats">Bord med 6 plasser:</div>
+</div>
+    <div class="tableOverview">
+
+        <div class="fourSeats">
+            <div class="col1">
+                <div class="box" onclick="selectTable('a')">A</div>
+                <div class="box" onclick="selectTable('b')">B</div>
+                <div class="box" onclick="selectTable('c')">C</div>
+                <div class="box" onclick="selectTable('d')">D</div>
+                <div class="box" onclick="selectTable('e')">E</div>
+                
+            </div>
+
+            <div class="col2">
+                <div class="box" onclick="selectTable('f')">F</div>
+                <div class="box" onclick="selectTable('g')">G</div>
+                <div class="box" onclick="selectTable('h')">H</div>
+                <div class="box" onclick="selectTable('i')">I</div>
+                <div class="box" onclick="selectTable('j')">J</div>
+            </div>
+        </div>
+        <div class="sixSeats">
+            <div class="col1">
+                <div class="box" onclick="selectTable('k')">K</div>
+                <div class="box" onclick="selectTable('l')">L</div>
+                <div class="box" onclick="selectTable('m')">M</div>
+                <div class="box" onclick="selectTable('n')">N</div>
+                <div class="box" onclick="selectTable('o')">O</div>
+            </div>
+            <div class="col2">
+                <div class="box" onclick="selectTable('p')">P</div>
+                <div class="box" onclick="selectTable('q')">Q</div>
+                <div class="box" onclick="selectTable('r')">R</div>
+                <div class="box" onclick="selectTable('s')">S</div>
+                <div class="box" onclick="selectTable('t')">T</div>
+            </div>
+        </div>
+    </div>
+</div>
     `;
     document.getElementById('app').innerHTML = html;
 }
